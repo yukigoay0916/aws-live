9d1538d37a778493cb53841c993c4f20a858159b
yukigoay0916 <72856168+yukigoay0916@users.noreply.github.com>
2022-09-21 12:22:21 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    return render_template('test.html')
 
 
 @app.route("/about", methods=['POST'])
@@ -78,7 +78,7 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
+    return render_template('test.html', name=emp_name)
 
 
 if __name__ == '__main__':
